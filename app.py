385db89741a1c2fbd4d3f54a8e72d3d47d48f69c
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-08-30 08:24:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask , render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import game as g
+
+app = Flask(__name__)
+
+@app.route('/' , methods = ["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == "POST":
+        value = request.form["value"]
+        score = str(g.game(value))
+    return render_template('index.html', s = score)
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 8000)
