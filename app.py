e4c5b4ecc3298cd2d051662c9ba0762f64d09889
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-08-30 13:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def submit():
     return render_template('index.html', s = score)
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 8000)
+    app.run()
+    # app.run(debug=True, port = 8000)
