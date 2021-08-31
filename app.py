4e01fd12d988df271931066aa562acb23f324c08
Adarsh Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-08-31 20:07:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def submit():
 if __name__ == '__main__':
     # app.run(debug = False)
     # app.run(debug=True, port = 8000)
-    app.run()
+    app.run(host = '0.0.0.0', port = 88)
     # app.run(debug=False, port = 8000)
