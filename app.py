e1007a49492fb7a713707a5de2a0a236ed19b862
Rishikesh-kumar-7258 <69416888+Rishikesh-kumar-7258@users.noreply.github.com>
2021-09-01 17:16:57 +0530

    Added some features

--- app.py
+++ app.py
@@ -5,20 +5,27 @@ import game as g
 
 app = Flask(__name__)
 
+point = 0
+
 @app.route('/' , methods = ["GET"])
 def index():
     return render_template("index.html")
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    global point
     if request.method == "POST":
         value = request.form["value"]
-        score = str(g.game(value))
-    return render_template('submit.html', s = score)
+        print(value)
+        scores = g.game(value)
+        score = str(scores[0])
+        point += scores[1]
+        points = str(point)
+    return render_template('index.html', s = score, p = points)
 
 if __name__ == '__main__':
     # app.run(debug = False)
-    # app.run(debug=True, port = 8000)
+    app.run(debug=True, port = 8000)
     # app.run(host = '0.0.0.0', port = 88)
-    app.run()
+    # app.run()
     # app.run(debug=False, port = 8000)
--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import random
 
 def game(player):
+    global score
     rand = random.randint(1,3)
     if player == 's' or player == 'S':
-        player = 1 
+        player = 1
     elif player == 'p' or player == 'P':
         player = 2
     elif player == 'x' or player == 'X':
@@ -11,9 +12,9 @@ def game(player):
     else:
         return "Invalid Input!"
     if (player == 1 and rand == 1) or (player == 2 and rand == 2) or (player == 3 and rand == 3):
-        return "Draw"
+        return "Draw", 0
     elif (player == 2 and rand == 1) or (player == 3 and rand == 2) or (player == 1 and rand == 3):
-        return "You Won!"
+        return "You Won!", 1
     elif (player == 1 and rand == 2) or (player == 2 and rand == 3) or (player == 3 and rand == 1):
-        return "Lost!"
+        return "You Lost!", -1
 
